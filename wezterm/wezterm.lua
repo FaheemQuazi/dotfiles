a65af783dba77cc11e3e3b0f7926e91d791dbbd5
Faheem Quazi <faheemquazi@gmail.com>
2024-09-20 18:30:13 -0500

    merge laptop config with desktop

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 local act = wezterm.action
 local config = {}
 
+-- right click to paste
 config.mouse_bindings = {
     {
         event = { Down = { streak = 1, button = "Right" } },
@@ -18,7 +19,34 @@ config.mouse_bindings = {
     },
 }
 
-config.enable_tab_bar = false
+-- Statusbar
+wezterm.on('update-right-status', function(window, pane)
+    -- "Wed Mar 3 08:14"
+    local date = wezterm.strftime '%a %b %-d %H:%M '
+
+    local bat = ''
+    for _, b in ipairs(wezterm.battery_info()) do
+        bat = ' ' .. string.format('%.0f%%', b.state_of_charge * 100)
+    end
+
+    window:set_right_status(wezterm.format {
+        { Text = date },
+    })
+
+    window:set_left_status(wezterm.format {
+        { Text = ' ' .. wezterm.hostname() .. ' [' .. bat .. ' ]' },
+    })
+end)
+
+-- Tab Bar
+config.use_fancy_tab_bar = false
+config.enable_tab_bar = true
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+
+-- Colors
 config.color_scheme = 'Urple'
+config.font_size = 15.0
+
 
 return config
